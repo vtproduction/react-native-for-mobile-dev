6a39f7fc53ca024782deb096adb19f1f146d8de4
NIEN-TOMO <nienbkict@gmail.com>
2019-10-09 17:59:48 +0700

    change somes

--- ch3/src/TodoApp.js
+++ ch3/src/TodoApp.js
@@ -1,32 +1,30 @@
 import React, { Component } from "react";
 import {
-    View,
-    Text,
-    StyleSheet
+	View,
+	Text,
+	StyleSheet
 } from "react-native";
 import AddTodo from './containers/AddTodo'
 import VisibleTodos from './containers/VisibleTodos'
 class TodoApp extends Component {
 
+	render() {
+		return (
+			<View style={styles.container}>
+				<AddTodo />
+				<View>
+					<VisibleTodos />
+				</View>
+			</View>
+			);
+		}
+	}
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <AddTodo />
-
-                <View>
-                    <VisibleTodos />
-                </View>
-            </View>
-        );
-    }
-}
 export default TodoApp;
-
+	
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 40
-
-    }
+	container: {
+		flex: 1,
+		paddingTop: 40		
+	}
 });
